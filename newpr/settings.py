4527c6120673ea9a972c481e3b04bb52673d254e
user <sotvoldiyevazamat193@gmail.com>
2024-06-09 22:12:05 +0500

    deplosysssss

--- newpr/settings.py
+++ newpr/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASS'),
+        'PASS': config('DB_PASS'),
         'HOST': config('DB_HOST'),
         'PORT': config('DB_PORT'),
     }
